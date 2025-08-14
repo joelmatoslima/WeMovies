c3c0d292ac72a3e08e2f6547a19cc2513fcaecae
Joel Lima <joel@pippa.com.br>
2025-08-14 00:37:32 -0300

    Initialize React Router project with Docker support, ESLint configuration, and basic app structure. Added essential files including Dockerfile, package.json, and initial routes. Configured TypeScript and Vite for development and production environments.

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,13 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+
+      <h1>123</h1>
+   
+    </main>
+  );
+}
+
