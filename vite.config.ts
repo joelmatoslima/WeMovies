1ceab82f5df74cc50eab74a2cdb67f713b9356f9
Joel Lima <joel@pippa.com.br>
2025-08-14 22:28:16 -0300

    Remove Tailwind CSS plugin from Vite configuration to streamline build process.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
